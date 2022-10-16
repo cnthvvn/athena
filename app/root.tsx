3ef626cb9f5787937f535cd704cd24937d69b6d4
cynthiavovan <cynthiavovan@gmail.com>
2022-10-16 14:23:59 +0200

    init project

---
+++ app/root.tsx
@@ -0,0 +1,42 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet preload prefetch",
+    href: tailwindStylesheetUrl,
+    as: "style",
+  },
+];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,13 @@
+export default function Index() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center space-y-12">
+      <img
+        src="https://res.cloudinary.com/dabwjor9a/image/upload/v1665921544/Avatar_t5qqyr.png"
+        alt="Avatar Cynthia Vo Van"
+        className="max-w-xs"
+      />
+
+      <h1 className="text-4xl font-bold text-amber-500">C'est le feu 🔥</h1>
+    </div>
+  );
+}
